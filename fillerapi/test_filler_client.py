0d47f963c1835adce7b78784176117a85fba55a9
Rodrigo Mendez <rodmendezp@gmail.com>
2018-10-26 23:59:22 -0300

    added testings

--- fillerapi/test_filler_client.py
+++ fillerapi/test_filler_client.py
@@ -9,7 +9,17 @@ class TestRepitClient(TestCase):
         super().setUp()
 
     def test_get_filler_game(self):
-        self.client.filler_game.get_objects()
+        games = self.client.filler_game.get_objects()
+        pass
+
+    def test_get_filler_streamer(self):
+        streamers = self.client.filler_streamer.get_objects()
+        pass
+
+    def test_get_game_filtered_streamers(self):
+        streamers = self.client.filler_streamer.get_game_default_streamers('Fortnite')
+        pass
+
 
 
 if __name__ == '__main__':
