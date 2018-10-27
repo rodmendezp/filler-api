87986c11146888dc8ad8f8a05539708f4d2f975f
Rodrigo Mendez <rodmendezp@gmail.com>
2018-10-27 01:22:03 -0300

    removed response.json in get

--- fillerapi/api/base.py
+++ fillerapi/api/base.py
@@ -12,13 +12,11 @@ class FillerAPI(object):
         }
         return headers
 
-    def _request_get(self, path, params=None, json=True, url=BASE_URL):
+    def _request_get(self, path, params=None, url=BASE_URL):
         url = urljoin(url, path)
         headers = self._get_request_headers()
         response = requests.get(url, params=params, headers=headers)
         response.raise_for_status()
-        if json:
-            return response.json()
         return response
 
     def _request_post(self, path, data=None, params=None, url=BASE_URL):
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='fillerapi',
-      version='0.112',
+      version='0.12',
       description='API Client for Filler',
       author='Rodrigo Mendez',
       author_email='rodmendezp@gmail.com',
